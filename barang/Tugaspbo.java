401a522336de811532f4c4963f7f63c042e333a2
Mardianto04 <113657328+Mardianto04@users.noreply.github.com>
2023-06-11 16:23:10 +0800

    Add files via upload

---
+++ barang/Tugaspbo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package barang;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tugaspbo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        login loginFrame = new login();
+        loginFrame.setVisible(true);
+        loginFrame.pack();
+        loginFrame.setLocationRelativeTo(null); 
+    }
+    
+}
